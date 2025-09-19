386ee0ff5d3943b89bae1c7a786f0141213ebb73
stevoh607 <stephenndungu230@gmail.com>
2025-09-19 14:28:32 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,17 @@
+/*
+name. stephen ndungu
+reg:CT101/G/26448/25
+c program for units taken 
+*/
+#include<stdio.h>
+int main(){
+	
+	printf("anlague electronics\n ");
+	printf("introduction to programming and problem solving \n");
+	printf("communicatin skills\n");
+	printf("physics\n");
+	printf("mathematics for science\n");
+	printf("fundamentals of ict\n");
+	printf("introduction to computer architecture");
+	return 0;
+}
