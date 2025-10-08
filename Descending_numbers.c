881fb1d283bd6e70ef1d09de8d130252faed8613
stevoh607 <stephenndungu230@gmail.com>
2025-10-08 22:21:02 +0300

    Add files via upload

---
+++ Descending_numbers.c
@@ -0,0 +1,27 @@
+/*
+
+Name:Stephen ndungu
+Reg no:CT101/G/26448/25
+Description:a simple c program  to print 100_50 in descending order
+
+*/
+
+
+#include<stdio.h>  
+
+int main(){
+	
+	int i;
+	
+	
+	for( i=100; i>=50;i--){
+	
+	printf("%d\n",i);
+	
+	}
+	
+	
+return 0;	
+	
+	
+}
